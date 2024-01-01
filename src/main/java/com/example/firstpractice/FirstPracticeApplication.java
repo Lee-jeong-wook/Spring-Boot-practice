bf9b0d706e59f05469d3a2e9779883212e6e52e7
Lee-jeong-wook <ijeonguk55@gmail.com>
2024-01-01 20:35:22 +0900

    첫 커밋

---
+++ src/main/java/com/example/firstpractice/FirstPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstPracticeApplication.class, args);
+    }
+
+}
